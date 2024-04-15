d33d77f6e91a73f5ac013fc1609f8a22827c4a85
thatthep <thatthep@local.local>
2024-04-15 11:58:46 +0700

    Added Load Balancer. Support only HTTP. No HTTPS for the moment

--- main.tf
+++ main.tf
@@ -82,16 +82,96 @@ resource "aws_ecs_service" "nginx_service" {
   cluster         = aws_ecs_cluster.ecs_cluster.id
   task_definition = aws_ecs_task_definition.my_task.arn
   launch_type     = "FARGATE"
+  desired_count   = 3
   tags            = var.common_tags
 
   network_configuration {
     subnets          = data.aws_subnets.default.ids
     assign_public_ip = true
-    security_groups  = []
+    security_groups  = [aws_security_group.alb_sg.id]
   }
 
-  desired_count = 3
+  load_balancer {
+    target_group_arn = aws_lb_target_group.my_tg.arn
+    container_name   = "my-app"
+    container_port   = 80
+  }
+
   depends_on = [
+    aws_lb_listener.nginx_listener,
     aws_iam_role_policy_attachment.ecs_task_execution_role_policy
   ]
+}
+
+
+### Target Group and Load Balancer
+
+resource "aws_lb" "nginx_alb" {
+  name               = "my-nginx-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = data.aws_subnets.default.ids
+
+  enable_deletion_protection = false
+
+  tags = var.common_tags
+}
+
+resource "aws_lb_target_group" "my_tg" {
+  name     = "my-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+  target_type = "ip" #  "instance" if using EC2 launch type
+  tags = var.common_tags
+
+  health_check {
+    interval            = 30
+    path                = "/"
+    protocol            = "HTTP"
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+}
+
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Allow all inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+  tags = var.common_tags
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_listener" "nginx_listener" {
+  load_balancer_arn = aws_lb.nginx_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+    default_action {
+      type             = "forward"
+      target_group_arn = aws_lb_target_group.my_tg.arn
+    }
 }
